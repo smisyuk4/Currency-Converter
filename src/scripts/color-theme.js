96ebb53f96967c2b1ae0e7d65f09982156301f28
Sergey Misyuk <smisyuk@gmail.com>
2022-11-23 23:46:48 +0200

    bug w/icon

--- src/scripts/color-theme.js
+++ src/scripts/color-theme.js
@@ -10,10 +10,10 @@ initialTheme('light-theme')
 refs.buttonColorTheme.addEventListener('click', onClickButtonCT)
 
 function onClickButtonCT() {
-    refs.iconNight.toggleAttribute('hide')
     refs.iconLight.toggleAttribute('hide')
+    refs.iconNight.toggleAttribute('hide')  
 
-    if (localStorage.getItem('theme') === 'light-theme') {        
+    if (localStorage.getItem('theme') === 'light-theme') {   
         initialTheme('night-theme') 
     } else {
         initialTheme('light-theme') 
